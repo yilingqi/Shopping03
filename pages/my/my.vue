623608d0a87caf582b683c4e47db13441f4979da
onezeroseven <1774573689@qq.com>
2022-11-30 22:25:14 +0800

    完成了个人主页和支付

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,12 +1,14 @@
 <template>
 	<view>
-		My
+		<my-login v-if="!token"></my-login>
+		<my-userinfo v-else></my-userinfo>
 	</view>
 </template>
 
 <script>
 // 导入自己封装的 mixin 模块
 	import badgeMix from '@/mixins/tabbar-badge.js'
+	import {mapState} from 'vuex'
 	export default {
 		 // 将 badgeMix 混入到当前的页面中进行使用
 		mixins: [badgeMix],
@@ -14,10 +16,17 @@
 			return {
 				
 			};
+		},
+		computed:{
+			 // 2. 从 m_user 模块中导入需要的 token 字符串
+			    ...mapState('m_user', ['token']),
 		}
 	}
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
